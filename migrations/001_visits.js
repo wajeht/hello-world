1765684da98ff33f094d62878632b528c1413d58
wajeht <58354193+wajeht@users.noreply.github.com>
2026-02-22 21:45:38 -0600

    chore: add gitignore and fix formatting

--- migrations/001_visits.js
+++ migrations/001_visits.js
@@ -1,10 +1,10 @@
 export function up(knex) {
-  return knex.schema.createTable('visits', (table) => {
-    table.increments('id');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
+  return knex.schema.createTable("visits", (table) => {
+    table.increments("id");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
   });
 }
 
 export function down(knex) {
-  return knex.schema.dropTable('visits');
+  return knex.schema.dropTable("visits");
 }
